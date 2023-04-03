c6040bf218f6d5bab849daf0bf9964f17cc64d23
delta-bravo277 <sanketlawande1@gmail.com>
2023-04-03 19:14:03 +0530

    UI Changed #patch

--- src/components/UI/ErrorAlert.js
+++ src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Something went wrong! but Application running on S3</h2>
+      <h2>Something went wrong! but Application running on Cloudfront using S3</h2>
       <p>{props.errorText}</p>
     </section>
   );
