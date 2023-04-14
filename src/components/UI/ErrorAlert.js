c15c343dca61498726554936553ec15063d81f72
delta-bravo277 <sanketlawande1@gmail.com>
2023-04-15 00:23:17 +0530

    update-distribution with wait but seq changed #minor

--- src/components/UI/ErrorAlert.js
+++ src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Application running on CLOUDFRONT using S3 WITH UPDATE-DISTRIBUTION WITHOUT WAITING</h2>
+      <h2>Application running on CLOUDFRONT using S3 WITH UPDATE-DISTRIBUTION WITH WAITING , SEQ CHANGED</h2>
       <p>{props.errorText}</p>
     </section>
   );
