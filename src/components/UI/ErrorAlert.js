9b2447ff18e4f02d53524ac068c3b73058f1704d
sanketl-talentica <sanket.lawande@talentica.com>
2023-04-14 16:59:36 +0530

    cloudfront point

--- src/components/UI/ErrorAlert.js
+++ src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Application running on Cloudfront using S3</h2>
+      <h2>Application running on Cloudfront using S3 with tagged folder</h2>
       <p>{props.errorText}</p>
     </section>
   );
