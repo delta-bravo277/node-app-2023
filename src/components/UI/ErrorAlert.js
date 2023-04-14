c28a27300e5a41b56d89e9c0507e84b075168262
delta-bravo277 <sanketlawande1@gmail.com>
2023-04-15 00:34:02 +0530

    update-distribution test1 #patch

--- src/components/UI/ErrorAlert.js
+++ src/components/UI/ErrorAlert.js
@@ -5,7 +5,7 @@ import './ErrorAlert.css';
 function ErrorAlert(props) {
   return (
     <section className='error-alert'>
-      <h2>Application running on CLOUDFRONT using S3 WITH UPDATE-DISTRIBUTION WITH WAITING , SEQ CHANGED and without cache</h2>
+      <h2>Application running on CLOUDFRONT using S3 WITH UPDATE-DISTRIBUTION WITH WAITING , SEQ CHANGED and without cache test1</h2>
       <h3>test1</h3>
       <p>{props.errorText}</p>
     </section>
